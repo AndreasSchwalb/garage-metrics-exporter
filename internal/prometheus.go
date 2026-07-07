b5e1ba15c99e286871a6ab78f7c732a81ac433c7
Raido Kalbre <rkalbre@gmail.com>
2026-07-07 09:26:57 +0300

    Changed the exported metric's help message

--- internal/prometheus.go
+++ internal/prometheus.go
@@ -13,7 +13,7 @@ func NewMetrics(reg prometheus.Registerer) *Metrics {
 	m := &Metrics{
 		Bytes: promauto.With(reg).NewGaugeVec(prometheus.GaugeOpts{
 			Name: "garage_bytes_total",
-			Help: "The total number of processed events",
+			Help: "The size of a bucket in bytes",
 		}, []string{"bucket"}),
 	}
 	return m
